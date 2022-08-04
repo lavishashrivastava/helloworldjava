59004d370135bf4f3dfd40415d7e7dae07887072
Lavisha Shrivastava <33374217+lavishashrivastava@users.noreply.github.com>
2022-08-04 17:34:00 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World updated on 29th july");
+    System.out.println("Hello World updated on github using webhooks");
     }
 }
